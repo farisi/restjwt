24883831a8e055e38f6797eb4ac1bf8368d9c7ea
salman <salmandriva@gmail.com>
2024-11-25 17:00:10 +0700

    test aja

---
+++ src/main/java/com/practice/sf/rest/dtos/LoginUserDto.java
@@ -0,0 +1,9 @@
+package com.practice.sf.rest.dtos;
+
+public class LoginUserDto {
+    private String email;
+    
+    private String password;
+    
+    // getters and setters here...
+}
---
+++ src/main/java/com/practice/sf/rest/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.practice.sf.rest.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.practice.sf.rest.entities.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
