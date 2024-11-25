96c1700be07d64ba669d3e809874c76ea335fd08
salman <salmandriva@gmail.com>
2024-11-25 16:26:12 +0700

    first commit

---
+++ src/test/java/com/practice/sf/rest/RestjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sf.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
